4122a124e2c7f14fd5d429454899776d828b7379
İlker Mercan <ilkermercan@outlook.com>
2021-02-18 00:11:15 +0300

    17 ŞUBAT DERS SONU

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,13 +1,17 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Concrete;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using static Core.Aspects.Autofac.Validation.Class1;
 
 namespace Business.Concrete
 {
@@ -20,16 +24,13 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
-
-            if (product.ProductName.Length<2)
-            {
-                //Magic strings
-                return new ErrorResult(Messages.ProductNameInvalid); 
-            }
-
+            //validation          
             //İş Kodları = Business codes
+       
+
             _productDal.Add(product);
 
             return new SuccessResult(Messages.ProductAdded);
